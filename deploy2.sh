88dced0be35a79f7736ca77d7dc63620911c7c73
aranwhite <aranw@me.com>
2018-08-27 14:06:09 -0600

    Update deploy2.sh

--- deploy2.sh
+++ deploy2.sh
@@ -1,15 +1,14 @@
 JSON='{"username": "admin", "password": "Password1"}'
 echo $JSON
 
-apiKey=$(curl -i --header "Content-Type: application/json" --data '{"username": "admin", "password": "Password1"}' http://localhost:8080/rest/abl/admin/v2/@authentication| sed -n 's/.*"apikey": "\(.*\)",/\1/p' | sed 's/"//g' | sed "s/[,].*//")
+apiKey=$(curl -i --header "Content-Type: application/json" --data '{"username": "admin", "password": "Password1"}' http://34.210.71.26:8080/rest/abl/admin/v2/@authentication| sed -n 's/.*"apikey": "\(.*\)",/\1/p' | sed 's/"//g' | sed "s/[,].*//")
 
 echo APIKEY: $apiKey
 
-projectID=$(curl -i --header "Content-Type: application/json" --header "Authorization:CALiveAPICreator "${apiKey}":1" --data '{"name": "Vista","url_name": "Vista","is_active": true,"account_ident": 1000,"authprovider_ident": 1000 }' http://localhost:8080/rest/abl/admin/v2/admin:projects | tr -d '\n'|sed 's/ //g'|sed 's@.*:@@'|sed 's/}.*//')
+projectID=$(curl -i --header "Content-Type: application/json" --header "Authorization:CALiveAPICreator "${apiKey}":1" --data '{"name": "Vista","url_name": "Vista","is_active": true,"account_ident": 1000,"authprovider_ident": 1000 }' http://34.210.71.26:8080/rest/abl/admin/v2/admin:projects | tr -d '\n'|sed 's/ //g'|sed 's@.*:@@'|sed 's/}.*//')
 
 
 echo projectID: $projectID
  
 
-curl -i --header "Content-Type:application/json" --header "Authorization:CALiveAPICreator "${apiKey}":1" --data \{\"name\":\"CustomerDataSource\",\"role_strategy\":\"\simplicity\",\"schema_editable\":false,\"active\":true,\"project_ident\":"${projectID}",\"dbasetype_ident\":23,\"comments\":\"Created\ using\ Connect\ Wizard\",\"url\":\"jdbc\:csv:Data\ Source=/Users/whiar01/Desktop\;Include\ Files=.csv\;Extended\ Properties=LineEnding=LF\;HDR=Yes\;FMT=CSVDelimited\"\} http://localhost:8080/rest/abl/admin/v2/DbSchemas -v
-#echo {\"name\":\"CustomerDataSource\",\"role_strategy\":"\simplicity\",\"schema_editable\":false,\"active\":true,\"project_ident\":"${projectID}",\"dbasetype_ident\":23,\"comments\":\"Created\ using\ Connect\ Wizard\",\"url\":\"jdbc:csv:Data\ Source=/Users/whiar01/Desktop;Include\ Files=.csv;Extended\ Properties=LineEnding=LF;HDR=Yes;FMT=CSVDelimited\"}
+curl -i --header "Content-Type:application/json" --header "Authorization:CALiveAPICreator "${apiKey}":1" --data \{\"name\":\"CustomerDataSource\",\"role_strategy\":\"\simplicity\",\"schema_editable\":false,\"active\":true,\"project_ident\":"${projectID}",\"dbasetype_ident\":23,\"comments\":\"Created\ using\ Connect\ Wizard\",\"url\":\"jdbc\:csv:Data\ Source=/Users/whiar01/Desktop\;Include\ Files=.csv\;Extended\ Properties=LineEnding=LF\;HDR=Yes\;FMT=CSVDelimited\"\} http://34.210.71.26:8080/rest/abl/admin/v2/DbSchemas -v
